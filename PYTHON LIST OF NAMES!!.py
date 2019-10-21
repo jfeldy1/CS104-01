cc88e35c1e326f125f72ca172a4f25423b8bd2e4
jfeldy1 <55414195+jfeldy1@users.noreply.github.com>
2019-10-21 15:19:22 -0400

    Add files via upload

---
+++ PYTHON LIST OF NAMES!!.py
@@ -0,0 +1,17 @@
+names = ["Sam" , "Mikey" , "Parker" , "Andrew" , "Tyler" , "Keegan" , "Joe" , "Bob" , "Dylan" , "Zach"]
+n1 = input("enter first name")
+n2 = input("enter next name")
+names.append(n1)
+names.append(n2)
+print(names[0])
+search = input("enter search name:")
+if search in names:
+    print(search , "was found")
+else:
+    print(search , "was not found")
+if search is end:
+    print("End")
+    
+    
+    
+          
