3eb19c4d418c3679afc55cbe1c3236d56a75a381
jfeldy1 <55414195+jfeldy1@users.noreply.github.com>
2019-11-11 15:09:20 -0500

    Add files via upload

---
+++ PYTHON NUMBERS.py
@@ -0,0 +1,11 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 00:48:40) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>  numbers = ["1" , "2" , "3"  "4" , "5" , "6" , "7" , "8" , "9" , "10" , "11" , "12" , "13" , "14" "15" , "16" , "17" , "18" , "19" , "20"]
+n1 = input("enter first number")
+n2 = input("enter next number")
+numbers.append(n1)
+numbers.append(n2)
+print(numbers[0])
+search = input("enter first number:")
+sum =(n1 + n2)
